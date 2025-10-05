86029dafeae448a1b44f8f5f163715967d860787
TheMarc05 <corneamarc@gmail.com>
2025-10-06 00:56:28 +0300

    Problem 2 resolved

---
+++ src/Laborator1/Main.java
@@ -0,0 +1,60 @@
+package Laborator1;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void problema2() throws IOException {
+        List<Integer> nr = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader("src/Laborator1/in.txt"))){
+            String line;
+            while((line = br.readLine()) != null){
+                nr.add(Integer.parseInt(line.trim()));
+            }
+        } catch (IOException e) {
+            System.out.println("Eroare la citire" + e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        int s = 0;
+
+        int min = nr.get(0);
+        int max = nr.get(0);
+
+        for(int n : nr){
+            s += n;
+            if(n < min){
+                min = n;
+            }
+
+            if(n > max){
+                max = n;
+            }
+        }
+
+        System.out.println("Suma nr: " + s);
+        System.out.println("Nr minim: " + min);
+        System.out.println("Nr maxim: " + max);
+
+        double ma = (double) s / nr.size();
+
+        System.out.println("Media aritmetica: " + ma);
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("src/Laborator1/out.txt"))) {
+            bw.write("Suma: " + s + "\n");
+            bw.write("Nr minim: " + min + "\n");
+            bw.write("Nr maxim: " + max + "\n");
+            bw.write("Media");
+        } catch (IOException e){
+            System.out.println("Eroare la scriere in fisierfdfdf: " + e.getMessage());
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        problema2();
+    }
+
+}
